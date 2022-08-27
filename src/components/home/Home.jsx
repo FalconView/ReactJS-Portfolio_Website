7f39c06ce13524b65bc4eca4d7861b3468be0b26
Feroz Zargar <Ferozzargar@hotmail.com>
2022-08-27 02:08:48 -0400

    more css files were added to components

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./home.css";
+import Me from "../../assests/avatar-1.svg";
 
 const Home = () => {
-  return <div>Home</div>;
+  return;
+  <section className="home container" id="home">
+    <div className="intro">
+      <img src={Me} alt="" className="home__img" />
+      <h1 className="home__name"></h1>
+      <span className="home__education">I'm a Full Stack Developer</span>
+    </div>
+  </section>;
 };
 
 export default Home;

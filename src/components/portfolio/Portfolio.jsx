b33bcccff4c633b8895665217d7a6122bd566d9f
Feroz Zargar <Ferozzargar@hotmail.com>
2022-09-06 14:40:27 -0400

    Menu.jsx was created at components/portfolio and hard coded

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import "./portfolio.css";
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <section className="work container section" id="work">
+      <h2 className="section__title">Recent Works</h2>
+
+      <div className="work__filters">
+        <span className="work__item">Everything</span>
+        <span className="work__item">Creative</span>
+        <span className="work__item">Art</span>
+        <span className="work__item">Design</span>
+        <span className="work__item">Branding</span>
+      </div>
+
+      <div className="work__container grid"></div>
+    </section>
+  );
 };
 
 export default Portfolio;

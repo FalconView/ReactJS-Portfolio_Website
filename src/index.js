c70322c5fcd84cb8bf68f24fa377b9486d557a4a
Feroz Zargar <Ferozzargar@hotmail.com>
2022-09-09 01:47:42 -0400

    General debugging

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+require("dotenv").config();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, console.log(`Server running on port ${port}`));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
